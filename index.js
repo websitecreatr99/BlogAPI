2736f1101380c8753baef57b041d944c5883c6ef
Kishan Pahadiya <85162734+websitecreatr99@users.noreply.github.com>
2022-07-14 19:29:34 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+const port = process.env.PORT || 3000;
+const apiData = require("./data.json");
+
+app.get("/", (req,res) => {
+    res.send("Hello, I am live");
+});
+
+app.get("/blog", (req,res) => {
+    res.send(apiData);
+});
+
+app.listen(port, () => {
+    console.log("Hello, Everyone");
+})
